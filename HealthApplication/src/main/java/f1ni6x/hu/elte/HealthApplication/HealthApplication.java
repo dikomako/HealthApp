6caba23d9bf53256e5355599b51808023cb4d925
dikomako <43717646+dikomako@users.noreply.github.com>
2019-02-09 09:14:52 +0100

    Maven Project létrehozása
    Maven Project létrehozása

---
+++ HealthApplication/src/main/java/f1ni6x/hu/elte/HealthApplication/HealthApplication.java
@@ -0,0 +1,14 @@
+package f1ni6x.hu.elte.HealthApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApplication.class, args);
+	}
+
+}
+
